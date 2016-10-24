06fe2cd0fb0809da7b7e25a7de0d6d9a2027503b
Kevin Duong <kduong5@yahoo.com>
2016-10-24 14:59:39 -0700

    updated donate and learn page

--- js/home.js
+++ js/home.js
@@ -1,4 +1,15 @@
 var main = function() {
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 100) {
+        $('#top-nav').addClass('hide');
+        $('#top-bot-nav').addClass('fixed');
+        $('#top-bot-nav').addClass('opaque');
+    } else {
+        $('#top-nav').removeClass('hide');
+        $('#top-bot-nav').removeClass('fixed');
+        $('#top-bot-nav').removeClass('opaque');
+    }
+});
 };
 
 $(document).ready(main);
