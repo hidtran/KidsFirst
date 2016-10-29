6efa53d5992daa2f06e49884f624dd1f4647294d
Kevin Duong <kduong5@yahoo.com>
2016-10-28 23:56:22 -0700

    added custom error page

---
+++ error.php
@@ -0,0 +1,34 @@
+
+<!doctype html>
+<html>
+<head>
+  <title>Kids First OC - Error</title>
+  <?php include 'head.php' ?>
+
+</head>
+<body>
+
+<?php include 'header.php' ?>
+
+  <!-- Carousel
+   ================================================== -->
+   <div class="container">
+     <h2>404 Error</h2>
+     <h4>The page you requested no longer exists or is temporarily unavailable.</h3>
+       <div>
+         <p>
+          We apologize for this inconvenience. Please visit the <a href="http://kevinduong.me/kidsfirstoc">Kids First OC</a> homepage or use the links on this page to find the information you need.
+        </p>
+  </div>
+   </div>
+
+
+<?php include 'footer.php' ?>
+
+</body>
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="js/home.js"></script>
+</html>
